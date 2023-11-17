e77882ffcdfd49f7b300140ccb1995741b1d7aba
Umesh Chandra <umesh.sakinala20@gmail.com>
2023-11-17 12:58:25 +0530

    Minor Changes
    Signed-off-by: Umesh Chandra <umesh.sakinala20@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,19 @@ const studentRoute = require("./controller/studentRoute");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const dotenv = require("dotenv").config();
 
+const DB = process.env.DATABASE;
+const Port = process.env.PORT;
+mongoose.connect(DB, {useNewUrlParser: true})
 mongoose.set("strictQuery", true); // DeprecationWarning To supress warning
-mongoose.connect("mongodb+srv://admin:admin@cluster0.ofvmpdb.mongodb.net/schooldb")
-var db = mongoose.connection;
-db.on("open", () => {
-    console.log("Database connected");
-})
-db.on("error", () => {
-    console.log("Database error");
-})
+// var db = mongoose.connection;
+// db.on("open", () => {
+//     console.log("Database connected");
+// })
+// db.on("error", () => {
+//     console.log("Database error");
+// })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,6 +24,6 @@ app.use(cors());
 
 app.use("/studentRoute", studentRoute);
 
-app.listen(4000,() => {
-    console.log("Server is running on port 4000");
+app.listen(Port,() => {
+    console.log(`Server is running on port ${Port}`);
 })
